0145080ae099bc8f2f07b7b730f21912aaf6fd4d
maxim.novak <maxim.novak@gmail.com>
2012-02-20 23:07:22 +0000

    1. SLN file 2. protobuf via NuGet 3. Some constants updated - now no Error 400 4. Namespaces corrected 5. Tests Added

---
+++ AndroidMarketApi.Tests/BasicFunctionalutyTests.cs
@@ -0,0 +1,59 @@
+﻿using System.Configuration;
+using AndroidMarketApi.Entities.protobuf;
+using NUnit.Framework;
+
+namespace AndroidMarketApi.Tests
+{
+  [TestFixture]
+  public class BasicFunctionalutyTests
+  {
+    private string email;
+    private string password;
+
+    private MarketSession session;
+
+    [TestFixtureSetUp]
+    public void TestFixtureSetUp()
+    {
+      email = ConfigurationManager.AppSettings["email"];
+      password = ConfigurationManager.AppSettings["password"];
+    }
+
+    [SetUp]
+    public void Setup()
+    {
+      session = new MarketSession();
+
+      session.Login(email, password);
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+      session = null;
+      email = null;
+      password = null;
+    }
+
+
+    [Test]
+    public void AppsRequest()
+    {
+      string query = "maps";
+      AppsRequest appsRequest = new AppsRequest
+                                  {
+                                    Query = query, StartIndex = 0, EntriesCount = 10, WithExtendedInfo = true
+                                  };
+
+      var response = session.Get(appsRequest);
+    }
+
+    [Test]
+    public void CategoriesRequest()
+    {
+      CategoriesRequest categoriesRequest = new CategoriesRequest();
+
+      var response = session.Get(categoriesRequest);
+    }
+  }
+}
---
+++ AndroidMarketApi/Entities/Session/Constant.cs
@@ -0,0 +1,32 @@
+﻿namespace AndroidMarketApi.Entities.Session
+{
+  public class Constant
+  {
+    public class AccountType
+    {
+      public const string Hosted = "HOSTED";
+      public const string Google = "GOOGLE";
+      public const string HostedOrGoogle = "HOSTED_OR_GOOGLE";
+    }
+
+    public class Config
+    {
+      public const string Service = "android";
+      public const int ProtocolVersion = 2;
+    }
+
+    public class DefaultContext
+    {
+      public const int Unknown1 = 0;
+      public const int Version = 2009011;
+      public const string AndroidId = "0123456789123456";
+      public const string DeviceAndSdk = "passion:9";
+    }
+
+    public class Url
+    {
+      public const string Login = "https://www.google.com/accounts/ClientLogin";
+      public const string ApiRequest = "http://android.clients.google.com/market/api/ApiRequest";
+    }
+  }
+}
